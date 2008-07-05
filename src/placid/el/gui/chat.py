ad8c153fdff8e1296ba6453266c7bbbb48ac4676
Alex Collins <atc-@users.noreply.github.com>
2008-07-05 17:12:36 +0000

    bug fixes. proper sizing of window and components.

--- src/placid/el/gui/chat.py
+++ src/placid/el/gui/chat.py
@@ -20,28 +20,28 @@ class ChatGUI(object):
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.connect('destroy', self.destroy)
 		self.window.connect('delete_event', self.destroy)
-		self.window.set_size_request(645, 500)
+		self.window.set_size_request(645, 510)
 		self.window.set_border_width(5)
 
 		# assign the fd of our elconnection to gtk
 		gobject.io_add_watch(self.elc.fileno(), gobject.IO_IN | gobject.IO_PRI, self.__process_packets)
+		gobject.timeout_add(15000, self.__keep_alive)
 
-		self.chat_hbox = gtk.HBox(True, 0)
+		self.chat_hbox = gtk.HBox(False, 0)
 		self.chat_hbox.show()
-		self.vbox = gtk.VBox(True, 0)
+		self.vbox = gtk.VBox(False, 0)
 		self.vbox.show()
 
 		# setup the chat text area
 		self.chat_buff = gtk.TextBuffer()
-		self.append_chat('Welcome to Pyela-Chat, part of the Pyela toolset. Visit pyela.googlecode.com for more information')
 		self.chat_view = gtk.TextView(self.chat_buff)
-		self.chat_view.set_size_request(640, 380)
+		self.chat_view.set_size_request(640, 480)
 		self.chat_view.set_editable(False)
 		self.chat_view.set_wrap_mode(gtk.WRAP_WORD)
 		self.chat_view.show()
 		self.scrolled_win = gtk.ScrolledWindow()
-		self.scrolled_win.set_size_request(640, 680)
-		self.scrolled_win.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+		self.scrolled_win.set_size_request(640, 480)
+		self.scrolled_win.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 		self.scrolled_win.add(self.chat_view)
 		self.scrolled_win.show()
 
@@ -50,7 +50,7 @@ class ChatGUI(object):
 
 		# setup the chat input & send button
 		self.msg_txt = gtk.Entry(max=155)
-		self.msg_txt.set_size_request(200, self.msg_txt.get_size_request()[1])
+		self.msg_txt.set_size_request(600, self.msg_txt.get_size_request()[1])
 		self.msg_txt.connect('key_press_event', self.__keypress_send_msg)
 		self.msg_txt.show()
 		self.send_btn = gtk.Button('Send')
@@ -61,11 +61,14 @@ class ChatGUI(object):
 		self.vbox.pack_start(self.chat_hbox, False, False, 0)
 
 		self.window.add(self.vbox)
-		self.window.show()
+		self.window.set_title("Pyela Chat - %s@%s:%s" % (self.elc.username, self.elc.host, self.elc.port))
+		self.window.show_all()
+		self.append_chat('Welcome to Pyela-Chat, part of the Pyela toolset. Visit pyela.googlecode.com for more information')
 		gtk.main()
 
 	def append_chat(self, text):
 		self.chat_buff.insert(self.chat_buff.get_end_iter(), text)
+		self.chat_view.scroll_to_iter(self.chat_buff.get_end_iter(), 0)
 
 	def __keypress_send_msg(self, widget, event=None):
 		if event.keyval == gtk.keysyms.Return:
@@ -81,6 +84,12 @@ class ChatGUI(object):
 
 		self.elc.send(ELPacket(type, msg))
 		self.msg_txt.set_text("")
+		return True
+	
+	def __keep_alive(self):
+		"""keeps self.elc alive by calling its keep_alive function.
+		This is called automatically every 15 seconds by the gobject API"""
+		self.elc.keep_alive
 		return True
 
 	def __process_packets(self, widget, data=None):
