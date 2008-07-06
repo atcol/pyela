4ba581c7d205b9d2155d4ac3f4a266c580b0439c
Alex Collins <atc-@users.noreply.github.com>
2008-07-06 00:50:58 +0000

    reimplement split_str remove null chars on strings

--- src/placid/el/util/strings.py
+++ src/placid/el/util/strings.py
@@ -8,4 +8,24 @@ def strip_chars(str):
 		if ord(char) < 127+ELConstants.C_LBOUND or ord(char) > 127+ELConstants.C_UBOUND:
 			stripped_str += char
 
+	if ord(stripped_str[-1]) == 0:
+		stripped_str = stripped_str[:-1]
+
 	return stripped_str
+
+def split_str(str, max_len):
+	"""Split the given string into a list of strings small enough for RAW_TEXT messages
+		str - the string to split
+		max_len - the maximum length of any given string
+	"""
+	strs = []
+	if len(str) < max_len:
+		# short enough to send in one message
+		return [str]
+	else:
+		# string is too long, truncate to multiple strings
+		idx = 0
+		while idx < len(str):
+			strs.append(str[idx:idx + max_len])
+			idx += max_len
+	return strs
