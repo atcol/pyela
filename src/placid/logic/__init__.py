b0a72d94fb0f2143b969b65b8385f1201f14d2b3
Alex Collins <atc-@users.noreply.github.com>
2008-07-12 11:43:51 +0000

    Add copyright notice

---
+++ src/placid/logic/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2008 Alex Collins
+#
+# This file is part of Pyela.
+# 
+# Pyela is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Pyela is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Pyela.  If not, see <http://www.gnu.org/licenses/>.
+
