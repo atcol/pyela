0a28a11bd384346ef0fd478c1a2645754c65d9ec
Alex Collins <atc-@users.noreply.github.com>
2008-07-12 13:46:01 +0000

    Move the individual components to their own classes

--- src/placid/el/gui/chat.py
+++ src/placid/el/gui/chat.py
@@ -54,54 +54,12 @@ class ChatGUI(gtk.Window):
 		self.chat_hbox = gtk.HBox(False, 0)
 		self.chat_hbox.show()
 
-		# set-up the scrollable window
-		self.scrolled_win = gtk.ScrolledWindow()
-		self.scrolled_win.set_size_request(560, 480)
-		self.scrolled_win.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-		self.scrolled_win.show()
-
-		# setup the chat text area
-		self.chat_buff = gtk.TextBuffer()
-		self.chat_view = gtk.TextView(self.chat_buff)
-		self.chat_view.set_size_request(640, 480)
-		self.chat_view.set_editable(False)
-		self.chat_view.set_wrap_mode(gtk.WRAP_WORD)
-		self.chat_view.show()
-		self.scrolled_win.add(self.chat_view)
-		self.chat_hbox.pack_start(self.scrolled_win, True, True, 0)
-
-		# set-up the channel & buddy list vbox and the buddy list scroll win
-		self.blist_scrolled_win = gtk.ScrolledWindow()
-		self.blist_scrolled_win.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-		self.blist_scrolled_win.show()
-		self.tool_vbox = gtk.VBox(False, 0)
-
-		# set-up the channel list tree view
-		self.channel_list = gtk.ListStore(gobject.TYPE_STRING)
-		self.channel_tree = gtk.TreeView(self.channel_list)
-		self.channel_tree.set_size_request(self.channel_tree.get_size_request()[0], 85)
-		self.channel_tree.set_reorderable(True)
-		self.channel_tree.show()
-		self.cell_ren = gtk.CellRendererText()
-		self.cell_ren.set_property("visible", "TRUE")
-		self.channel_col = gtk.TreeViewColumn("Channels", self.cell_ren, markup=0)
-		self.channel_col.set_attributes(self.cell_ren, text=0)
-		self.channel_tree.append_column(self.channel_col)
-
-		# set-up the buddy list tree view
-		self.buddy_list = gtk.ListStore(gobject.TYPE_STRING)
-		self.buddy_tree = gtk.TreeView(self.buddy_list)
-		self.buddy_tree.connect('row-activated', self.__buddy_list_dclick)
-		self.buddy_tree.show()
-		self.buddy_cell_ren = gtk.CellRendererText()
-		self.buddy_cell_ren.set_property("visible", "TRUE")
-		self.buddy_col = gtk.TreeViewColumn("Buddies", self.buddy_cell_ren, markup=0)
-		self.buddy_tree.append_column(self.buddy_col)
-		self.blist_scrolled_win.add(self.buddy_tree)
-		self.tool_vbox.pack_start(self.channel_tree, False, False, 0)
-		self.tool_vbox.pack_start(self.blist_scrolled_win, True, True, 0)
+		self.chat_area = ChatArea()
+		self.chat_hbox.pack_start(self.chat_area, True, True, 0)
 
 		# add the chat & tool vbox to the chat hbox o,0
+		self.tool_vbox = ToolVBox()
+		self.tool_vbox.buddy_tree.connect('row-activated', self.__buddy_list_dclick)
 		self.vbox.pack_start(self.chat_hbox, False, False, 0)
 		self.chat_hbox.pack_end(self.tool_vbox, False, False, 0)
 
@@ -141,17 +99,16 @@ class ChatGUI(gtk.Window):
 
 		if response == 0:
 			# login crendials entered
-			#self.elc = ELConnection(l.user_txt.get_text(), l.
 			self.elc = ELConnection(l.user_txt.get_text(), l.passwd_txt.get_text(), l.host_txt.get_text(), int(l.port_txt.get_text()))
 			self.elc.connect()
 			l.destroy()
-		elif response == 1:
+		else:
 			# quit
 			sys.exit(0)
 
 	def append_chat(self, text):
-		self.chat_buff.insert(self.chat_buff.get_end_iter(), text)
-		self.chat_view.scroll_to_mark(self.chat_buff.get_insert(), 0)
+		self.chat_area.chat_buff.insert(self.chat_area.chat_buff.get_end_iter(), text)
+		self.chat_area.chat_view.scroll_to_mark(self.chat_area.chat_buff.get_insert(), 0)
 
 
 	def __keypress(self, widget, event=None):
@@ -195,8 +152,8 @@ class ChatGUI(gtk.Window):
 	def __buddy_list_dclick(self, buddy_tree, path, col, data=None):
 		"""User double-clicked a row in the buddy list treeview"""
 		# add /[name] if self.msg_txt is empty, otherwise append the name
-		iter = self.buddy_list.get_iter(path)
-		buddy = self.buddy_list.get_value(iter, 0)
+		iter = self.tool_vbox.buddy_list.get_iter(path)
+		buddy = self.tool_vbox.buddy_list.get_value(iter, 0)
 		if self.msg_txt.get_text() == "":
 			# add /[name]
 			self.msg_txt.set_text("/%s " % buddy)
@@ -220,25 +177,25 @@ class ChatGUI(gtk.Window):
 					self.append_chat("\n%s" % strip_chars(packet.data[1:]))
 			elif packet.type == ELNetFromServer.GET_ACTIVE_CHANNELS:
 				# active channel, c1, c2, c3
-				self.channel_list.clear()
+				self.tool_vbox.channel_list.clear()
 				chans = struct.unpack('<biii', packet.data)
 				i = 0
 				active = chans[0]
 				for c in chans[1:]:
 					if c != 0:
 						self.channels.append(Channel(c, i == active))
-						self.channel_list.append(["%s" % c])
+						self.tool_vbox.channel_list.append(["%s" % c])
 					i += 1
 			elif packet.type == ELNetFromServer.BUDDY_EVENT:
 				event = ord(packet.data[0])# 1 is online, 0 offline
 				if event == 1:
 					buddy = str(strip_chars(packet.data[2:]))
 					self.append_chat("\n%s logged in" % buddy)
-					self.buddy_list.append([buddy])
+					self.tool_vbox.buddy_list.append([buddy])
 				else:
 					buddy = str(strip_chars(packet.data[1:]))
 					self.append_chat("\n%s logged off" % buddy)
-					self.buddy_list.remove(self.find_buddy(buddy))
+					self.tool_vbox.buddy_list.remove(self.find_buddy(buddy))
 			elif packet.type == ELNetFromServer.LOG_IN_NOT_OK:
 				self.append_chat(strip_chars(packet.data))
 				self.elc.disconnect()
@@ -253,7 +210,7 @@ class ChatGUI(gtk.Window):
 		"""Get the gtk.Row where buddy is"""
 		if not buddy:
 			return None
-		for row in self.buddy_list:
+		for row in self.tool_vbox.buddy_list:
 			if row[0].upper() == buddy.upper():
 				return row
 		return None
@@ -269,3 +226,57 @@ class ChatGUI(gtk.Window):
 		gtk.main_quit()
 		return False
 
+class ChatArea(gtk.ScrolledWindow):
+	"""A gtk.ScrolledWindow that contains a chat view and buffer for 
+	raw text"""
+	def __init__(self):
+		# set-up the scrollable window
+		super(ChatArea, self).__init__()
+		#self.scrolled_win = gtk.ScrolledWindow()
+		#self.set_size_request(560, 480)
+		self.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		self.show()
+
+		# setup the chat text area
+		self.chat_buff = gtk.TextBuffer()
+		self.chat_view = gtk.TextView(self.chat_buff)
+		self.chat_view.set_size_request(640, 480)
+		self.chat_view.set_editable(False)
+		self.chat_view.set_wrap_mode(gtk.WRAP_WORD)
+		self.chat_view.show()
+		self.add(self.chat_view)
+
+class ToolVBox(gtk.VBox):
+	"""A vertical gtk.Box that contains the channel and buddy list TreeView objects"""
+	def __init__(self):
+		super(ToolVBox, self).__init__()
+		# set-up the channel & buddy list vbox and the buddy list scroll win
+		self.blist_scrolled_win = gtk.ScrolledWindow()
+		self.blist_scrolled_win.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		self.blist_scrolled_win.show()
+		#self.vbox = gtk.VBox(False, 0)
+
+		# set-up the channel list tree view
+		self.channel_list = gtk.ListStore(gobject.TYPE_STRING)
+		self.channel_tree = gtk.TreeView(self.channel_list)
+		self.channel_tree.set_size_request(self.channel_tree.get_size_request()[0], 85)
+		self.channel_tree.set_reorderable(True)
+		self.channel_tree.show()
+		self.cell_ren = gtk.CellRendererText()
+		self.cell_ren.set_property("visible", "TRUE")
+		self.channel_col = gtk.TreeViewColumn("Channels", self.cell_ren, markup=0)
+		self.channel_col.set_attributes(self.cell_ren, text=0)
+		self.channel_tree.append_column(self.channel_col)
+
+		# set-up the buddy list tree view
+		self.buddy_list = gtk.ListStore(gobject.TYPE_STRING)
+		self.buddy_tree = gtk.TreeView(self.buddy_list)
+		self.buddy_tree.show()
+		self.buddy_cell_ren = gtk.CellRendererText()
+		self.buddy_cell_ren.set_property("visible", "TRUE")
+		self.buddy_col = gtk.TreeViewColumn("Buddies", self.buddy_cell_ren, markup=0)
+		self.buddy_tree.append_column(self.buddy_col)
+		self.blist_scrolled_win.add(self.buddy_tree)
+		self.pack_start(self.channel_tree, False, False, 0)
+		self.pack_start(self.blist_scrolled_win, True, True, 0)
+		self.show()
