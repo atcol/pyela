81bfc2e13857225e7254a9a0cdc7f1baa0f13506
Vegar Storvann <vegar@storvann.net>
2015-03-23 22:25:26 +0100

    fix typo in chat client's setup.py

--- chat/setup.py
+++ chat/setup.py
@@ -3,11 +3,11 @@ from gui import VER_MINOR,VER_MAJOR,VER_EXTRA
 
 setup(
 	name='pyelachat',
-	version='{}.{}{}'.format(VER_MINOR,VER_MAJOR,VER_EXTRA),
+	version='{}.{}{}'.format(VER_MAJOR,VER_MINOR,VER_EXTRA),
 	description='Pyela Eternal-Lands chat client',
 	url='http://github.com/atc-/pyela',
-        author = 'Alex Collins, Vegar Storvann',
-        author_email = 'vegar@storvann.net',
+	author = 'Alex Collins, Vegar Storvann',
+	author_email = 'vegar@storvann.net',
 	packages=find_packages(),
 	entry_points = {
 		'gui_scripts': [
